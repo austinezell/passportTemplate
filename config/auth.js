3d39a5f51e31facfd2dc52ca9bc1981126dcee86
AustinEzell <austin.r.ezell@gmail.com>
2015-10-13 14:42:26 -0700

    creates auth middleware and begins to link frontend authentication

---
+++ config/auth.js
@@ -0,0 +1,7 @@
+'use strict'
+
+var jwt = require('express-jwt');
+
+var auth = jwt({secret: process.env.SECRET, userProperty: 'payload'});
+
+module.exports = auth;
--- public/js/models.js
+++ public/js/models.js
@@ -1,3 +1,17 @@
 'use strict';
 
 var app = angular.module('passportTest');
+
+app.factory('auth', ['$http', '$window', function($http, $window){
+  var auth = {};
+
+  this.saveToken = function (token){
+    $window.localStorage['flapper-news-token'] = token;
+  };
+
+  this.getToken = function (){
+    return $window.localStorage['flapper-news-token'];
+  }
+
+  return auth;
+}])
