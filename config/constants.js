fb7df9322d0ee8455f4b86a780526317ef7d753a
Austin Ezell <austin.r.ezell@gmail.com>
2016-03-17 00:14:48 -0700

    same

--- config/constants.js
+++ config/constants.js
@@ -1,5 +1,5 @@
 module.exports = {
   SECRET: (process.env.SECRET || "Secret"),
-  MONGO_URL: (process.env.MONGO_URL || process.env.MONGO_LAB_URI || "mongodb://localhost/test"),
+  MONGO_URL: (process.env.MONGO_URL || process.env.MONGOLAB_URI || "mongodb://localhost/test"),
   PORT: (process.env.PORT || 3000)
 }
